9a8a96e37e4ef874e4773808cea843cc95754c0e
solo.albayt@gmail.com <solo.albayt@gmail.com>
2024-04-14 14:09:23 +0700

    test global css 1

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,18 +1,19 @@
 import { NextResponse } from "next/server";
-import client from "@/db/db";
+// import client from "@/db/db";
 
 const dbName = 'b';
 
 export async function GET() {
   // Use connect method to connect to the server
-  await client.connect();
+  // await client.connect();
   console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
-  const a = await  collection.find({
-    user_id: "a"
-  }).toArray()
+  // const db = client.db(dbName);
+  // const collection = db.collection('documents');
+  // const a = await  collection.find({
+  //   user_id: "a"
+  // }).toArray()
 
+  const a = {}
   // the following code examples can be pasted here...
   console.log(a)
   return NextResponse.json(a)
