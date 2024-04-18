b2781233263824f7b6749b00b3ffcd486a9cc85d
AlbaytSolo <166964554+AlbaytSolo@users.noreply.github.com>
2024-04-18 20:28:34 +0700

    page.tsx

--- app/profile-info/page.tsx
+++ app/profile-info/page.tsx
@@ -26,31 +26,29 @@ const page = () => {
 
             <div>
               <h3 className='font-semibold text-lg'>NAMA PERTAMA</h3>
-              <p className='text-gray-50'>Fulan bin Fulano</p>
+              <input type='text' placeholder='Nama' name='name' className='px-4 py-2 w-[70%] border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2'/>
             </div>
 
             <div>
               <h3 className='font-semibold text-lg'>NOMOR TELEPON</h3>
-              <p className='text-gray-50'>+62 8123-4132-0982</p>
+              <input type='text' placeholder='Nomor Telepon' name='telp' className='px-4 py-2 w-[70%] border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2' />
             </div>
 
             <div>
               <h3 className='font-semibold text-lg'>ALAMAT</h3>
-              <p className='text-gray-50'>Jln. Kebangsaan No. 6 Jebres, Surakarta, Jawa Tengah</p>
+              <input type='text' placeholder='Alamat' name='telp' className='px-4 py-2 w-[70%] border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2' />
             </div>
 
             <div>
               <h3 className='font-semibold text-lg'>TENTANG ANDA</h3>
-              <p className='text-gray-50 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit natus officia totam fugit necessitatibus incidunt eaque quo est officiis voluptatibus. Totam molestias enim obcaecati suscipit vero, esse dolore. Ducimus repellendus aliquam architecto quidem maiores quisquam corporis cumque ab aperiam veritatis totam nisi, illo quasi deleniti ut amet nesciunt doloribus reiciendis.</p>
+              <textarea placeholder='Tentang Anda' name='about_me' rows={7} className='px-2 py-2 w-[70%] resize-none border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2'/>
             </div>
-
+            
             <div className='flex justify-end'>
-              <Link href={'/edit-profile-info'}>
-                <button className='flex items-center gap-2 px-4 py-2 bg-[#f13410] text-white rounded-lg duration-200 hover:bg-black'>
-                  <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" height={24} width={24} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className='fill-white'><path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" fill-rule="nonzero"/></svg>
-                  <p className='font-semibold'>Edit Profil</p>
-                </button>
-              </Link>
+              <button className='flex items-center gap-2 px-4 py-2 bg-[#f13410] text-white rounded-lg duration-200 hover:bg-black'>
+                <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" height={24} width={24} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className='fill-white'><path d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z" fill-rule="nonzero"/></svg>
+                <p className='font-semibold'>Edit Profil</p>
+              </button>
             </div>
           </div>
           
