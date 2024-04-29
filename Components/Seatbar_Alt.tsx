42efd3008115b25de71e4e6b985ac31a30352ac3
AlbaytSolo <solo.albayt@gmail.com>
2024-04-29 12:04:17 +0700

    add little change

--- Components/Seatbar_Alt.tsx
+++ Components/Seatbar_Alt.tsx
@@ -14,7 +14,7 @@ const Seatbar_Alt = ({totalSeat, remainingSeat}: seatBarProps) => {
       <div className='w-full h-10 bg-gray-20 rounded-lg relative'>
         <div className='flex items-center'>
           {/* Bar Progress */}
-          <div className='bg-[#f14310] h-10 rounded-xl absolute top-0 left-0' style={{ width: `${barPercentage}%` }} />
+          <div className='bg-[#f14310] h-10 rounded-lg absolute top-0 left-0' style={{ width: `calc(100% - ${barPercentage}%)` }} />
           <p className='whitespace-nowrap font-bold text-white m-2 relative z-10'>Sisa Seat: {remainingSeat}</p>
         </div>
       </div>
