e6ddea7fd13fa3aaa2339131d73bc7669dca1ac6
Aorus <alyzanadif04@gmail.com>
2024-05-03 09:06:17 +0700

    lemme cook cache

--- app/api/paket/route.ts
+++ app/api/paket/route.ts
@@ -1,7 +1,10 @@
-export const fetchCache = 'force-no-store';
 import { getPakets } from "@/db/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(await getPakets())
+  return NextResponse.json(await getPakets(), {
+    headers: {
+      "Cache-Control": "no-cache"
+    }
+  })
 }
