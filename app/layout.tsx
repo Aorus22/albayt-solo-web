d8be12d31c497c6a656caf923e31fdc5a1fffcc1
Aorus <alyzanadif04@gmail.com>
2024-05-03 17:16:00 +0700

    meta pixel for real

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
 import { AuthContextProvider } from "@/context/AuthContext";
 import React from "react";
-import Head from "next/head";
+import FacebookPixel from "@/Components/FacebookPixel";
 
 export const metadata: Metadata = {
     title: "Project Albayt",
@@ -15,29 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <Head>
-            <Script
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1733154043759153'); 
-              fbq('track', 'PageView');
-            `,
-                }}
-            />
-        </Head>
         <body>
         <AuthContextProvider>
             <Navbar />
             {children}
+            <FacebookPixel />
             <Footer />
         </AuthContextProvider>
         </body>
