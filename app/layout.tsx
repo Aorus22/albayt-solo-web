65b7116e8c38b4a36c2f4024caf0d91f897f9826
Aorus <alyzanadif04@gmail.com>
2024-05-03 15:03:31 +0700

    test metapixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,24 +15,22 @@ export default function RootLayout({children}: {  children: React.ReactNode;}) {
   return (
     <html lang="en">
     <Head>
-      <title>Halaman Saya</title>
-      <meta name="description" content="Deskripsi halaman saya." />
-      <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1733154043759153');
-          fbq('track', 'PageView');
-          `,
-          }}
-      />
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'YOUR_PIXEL_ID'); // Ganti dengan ID Pixel Anda
+              fbq('track', 'PageView');
+            `,
+            }}
+        />
     </Head>
     <body>
     <AuthContextProvider>
