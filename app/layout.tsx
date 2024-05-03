73586506d0e444092c76c67150820ae65ce9851a
Aorus <alyzanadif04@gmail.com>
2024-05-03 09:38:28 +0700

    add metapixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
 import {AuthContextProvider} from "@/context/AuthContext";
 import React from "react";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Project Albayt",
@@ -13,14 +14,26 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {  children: React.ReactNode;}) {
   return (
     <html lang="en">
-    <head>
-        {/*<script*/}
-        {/*    id = "meta-pixel"*/}
-        {/*    dangerouslySetInnerHTML={*/}
-        {/*        __html: ``*/}
-        {/*    }*/}
-        {/*/>*/}
-    </head>
+    <Head>
+      <title>Halaman Saya</title>
+      <meta name="description" content="Deskripsi halaman saya." />
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1733154043759153');
+          fbq('track', 'PageView');
+          `,
+          }}
+      />
+    </Head>
     <body>
     <AuthContextProvider>
         <Navbar/>
