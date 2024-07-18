00682a69d5e96320f52c5ba39d63998e272c5c2d
CukupBani <paus.biru015@gmail.com>
2024-07-18 13:40:24 +0700

    change informasi profil layout

--- app/info-profil/page.tsx
+++ app/info-profil/page.tsx
@@ -24,43 +24,69 @@ const Page = () => {
     }, [user]);
 
     return (
-        <section className='max-container padding-container py-8'>
-            <h1 className='font-bold text-5xl text-[#f14310] text-center'>PROFIL</h1>
-            <div className='flexCenter'>
-                <div className='grid md:grid-cols-2 my-8 max-w-5xl w-full'>
-                    <div className='flex flex-col justify-center items-center gap-5'>
-                        <ImageWithLoading src={(userData?.photo)?.replace("=s96-c", "=s1024")} alt='profil-photo' width={"240px"} height={"240px"} className='w-80 h-80 rounded-full object-cover'/>
+        <section className='p-20 flex items-center justify-center'>
+            {/* Main Container */}
+            <div className='bg-white rounded-lg w-[65%] border'>
+
+                {/* section title */}
+                <div className='bg-[#f14310] rounded-t-lg w-full px-12 py-4'>
+                    <h2 className='font-bold text-white text-2xl uppercase tracking-wide'>Informasi Profil</h2>
+                </div>
+                {/* end of section title */}
+
+                {/* content */}
+                <div className='p-12'>
+
+                    {/* non-fillable section */}
+                    <div className='flex items-center gap-8'>
+                        <ImageWithLoading src={(userData?.photo)?.replace("=s96-c", "=s1024")} alt='profil-photo' width={"240px"} height='auto' className='w-44 h-44 rounded-full object-cover'/>
+                        <div className='flex flex-col gap-2'>
+                            <h2 className='text-[#f14310] font-bold text-3xl'>{userData?.name}</h2>
+                            <p className='text-gray-30 font-medium text-[16px]'>{userData?.email}</p>
+                        </div>
                     </div>
-                    <div className='flex flex-col gap-7'>
-                        <div>
-                            <h3 className='font-semibold text-lg'>EMAIL</h3>
-                            <p className='text-gray-50'>{userData?.email}</p>
+                    {/* end of non-fillable section */}
+
+                    {/* fillable section */}
+                    <form className='flex flex-col gap-8 mt-12'>
+                        <div className='flex flex-col gap-2'>
+                            <p className='uppercase font-medium text-lg'>Email</p>
+                            <input className='rounded-lg border px-4 py-2 text-gray-30 border-gray-20' type='text' value={userData?.email} readOnly/>
                         </div>
-                        <div>
-                            <h3 className='font-semibold text-lg'>NAMA</h3>
-                            <input type='text' placeholder='Nama' name='name' value={userData?.name} className='px-4 py-2 w-full border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2'/>
+
+                        <div className='flex flex-col gap-2'>
+                            <p className='uppercase font-medium text-lg'>Nama</p>
+                            <input className='rounded-lg border px-4 py-2 border-gray-20 focus:outline-none' type='text' name='name' value={userData?.name}/>
                         </div>
-                        <div>
-                            <h3 className='font-semibold text-lg'>NOMOR TELEPON</h3>
-                            <input type='text' placeholder='Nomor Telepon' name='telp' className='px-4 py-2 w-full border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2' />
+
+                        <div className='flex flex-col gap-2'>
+                            <p className='uppercase font-medium text-lg'>Nomor Telepon</p>
+                            <input className='rounded-lg border px-4 py-2 border-gray-20 focus:outline-none' type='text' name='telp' value={userData?.telp}/>
                         </div>
-                        <div>
-                            <h3 className='font-semibold text-lg'>ALAMAT</h3>
-                            <input type='text' placeholder='Alamat' name='alamat' className='px-4 py-2 w-full border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2' />
+
+                        <div className='flex flex-col gap-2'>
+                            <p className='uppercase font-medium text-lg'>Alamat</p>
+                            <input className='rounded-lg border px-4 py-2 border-gray-20 focus:outline-none' type='text' name='address' value={userData?.address}/>
                         </div>
-                        <div>
-                            <h3 className='font-semibold text-lg'>TENTANG ANDA</h3>
-                            <textarea placeholder='Tentang Anda' name='about_me' rows={7} className='px-2 py-2 w-full resize-none border border-gray-30 rounded-lg focus:outline-gray-30 focus:outline-2'/>
+
+                        <div className='flex flex-col gap-2'>
+                            <p className='uppercase font-medium text-lg'>Tentang Anda</p>
+                            <textarea className='rounded-lg border px-4 py-2 border-gray-20 focus:outline-none resize-none' rows={6} name='desc'>{userData?.desc}</textarea>
                         </div>
+
                         <div className='flex justify-end'>
                             <button className='flex gap-2 items-center px-4 py-2 bg-[#f14310] text-white rounded-lg duration-200 hover:bg-black'>
                                 <SaveIcon />
                                 <p>Simpan Perubahan</p>
                             </button>
                         </div>
-                    </div>
+                    </form>
+                    {/* end of fillable section */}
                 </div>
+                {/* end of content */}
+
             </div>
+            {/* end of main container */}
         </section>
     )
 }
