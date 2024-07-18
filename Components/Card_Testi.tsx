4bad5f83c1dedf01cc4f8a9df89e636d5b8b313c
CukupBani <paus.biru015@gmail.com>
2024-07-18 14:15:15 +0700

    fix testimoni card layout

--- Components/Card_Testi.tsx
+++ Components/Card_Testi.tsx
@@ -6,11 +6,9 @@ const Card_Testi = ({ testimoni }: { testimoni: Testimoni }) => {
   return (
     <div className='embla__slide m-2'>
         <div className='p-4 md:p-8 items-center bg-white h-full rounded-xl shadow-lg'>
-            <div className='flex items-center justify-center flex-col md:flex-row md:ml-8 my-2 rounded-sm'>
+            <div className='flex items-center justify-center md:justify-start flex-col md:flex-row gap-5 md:ml-8 my-2 rounded-sm'>
                 <img src={testimoni.img} alt='testi' className='w-16 h-16 object-cover rounded-full' />
-                <div className='flex w-full items-center justify-center text-center'>
-                    <div className='font-bold text-lg'>{testimoni.nama}</div>
-                </div>
+                <div className='font-bold text-lg text-center md:text-start'>{testimoni.nama}</div>
             </div>
             <div className='flex items-center justify-center md:justify-normal mt-4 mx-5 my-2 md:mr-5'>
                 <div className='flex gap-2 py-2 px-4 bg-[#89060b] rounded-full '>
